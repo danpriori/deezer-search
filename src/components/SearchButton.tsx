198e87efaaa81a314fb167be00ed9898124bdf33
Daniel Priori <daniel.priori@concise.ee>
2020-03-18 15:02:46 +0200

    prettier and add safeguard for artist undefined

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,49 +1,50 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import IState from '../interface/state';
+import IState from "../interface/state";
 
-import '../styles/SearchButton.scss';
-import { searchAlbums, getCurrentSelectionIndex, getArtistFromArtistListByIndex, getArtistListResult } from "../services/manageSearch";
+import "../styles/SearchButton.scss";
+import {
+  searchAlbums,
+  getCurrentSelectionIndex,
+  getArtistFromArtistListByIndex,
+  getArtistListResult
+} from "../services/manageSearch";
 
 class SearchButton extends Component {
-    
-    static contextTypes: { store: PropTypes.Requireable<object>; };
-    
-    handleSubmission = (e: any) => {
-        e.preventDefault();
-        const artistList = getArtistListResult();
-        const currentIndex = getCurrentSelectionIndex();
-        const artist = getArtistFromArtistListByIndex(currentIndex,artistList)[0];
-        searchAlbums(artist.id);
-    }
+  static contextTypes: { store: PropTypes.Requireable<object> };
 
-    render() {
-        
-        return (
-            <div className="search-button" onClick={this.handleSubmission} >
-                SEARCH
-            </div>
-        );
+  handleSubmission = (e: any) => {
+    e.preventDefault();
+    const artistList = getArtistListResult();
+    const currentIndex = getCurrentSelectionIndex();
+    const artist = getArtistFromArtistListByIndex(currentIndex, artistList)[0];
+    if (artist) {
+      searchAlbums(artist.id);
     }
+  };
+
+  render() {
+    return (
+      <div className="search-button" onClick={this.handleSubmission}>
+        SEARCH
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: IState) => {
-    return {
-        artistListResult: state.artistListResult,
-        searchInput: state.searchInput,
-        currentSelectionIndex: state.currentSelectionIndex,
-        albumListResult: state.albumListResult,
-        currentArtistSelected: state.currentArtistSelected
-    }
-}
-
+  return {
+    artistListResult: state.artistListResult,
+    searchInput: state.searchInput,
+    currentSelectionIndex: state.currentSelectionIndex,
+    albumListResult: state.albumListResult,
+    currentArtistSelected: state.currentArtistSelected
+  };
+};
 
 SearchButton.contextTypes = {
-    store: PropTypes.object
+  store: PropTypes.object
 };
-export default connect(
-    mapStateToProps,
-    undefined
-)(SearchButton);
+export default connect(mapStateToProps, undefined)(SearchButton);
